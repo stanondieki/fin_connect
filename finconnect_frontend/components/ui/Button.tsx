c5f2e34b99f94c8f8af22fa0fed652cfc8214f4d
AnthonyMakori <antonymakori2019@gmail.com>
2025-02-13 04:31:09 +0300

    refining Button

--- finconnect_frontend/components/ui/Button.tsx
+++ finconnect_frontend/components/ui/Button.tsx
@@ -1,20 +1,27 @@
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  variant?: "success" | "danger" | "primary" | "outline";
+  variant?: "success" | "danger" | "primary" | "outline" | "ghost";
+  size?: "small" | "medium" | "large";
 }
-
 
 const Button: React.FC<ButtonProps> = ({ children, className = "", variant = "primary", size = "medium", ...props }) => {
   const variantStyles = {
-    success: "bg-green-500 hover:bg-green-600",
-    danger: "bg-red-500 hover:bg-red-600",
-    primary: "bg-blue-500 hover:bg-blue-600",
-    outline: "border border-gray-500 text-gray-700 bg-white hover:bg-gray-100", // Add styles for outline
+    success: "bg-green-500 hover:bg-green-600 text-white",
+    danger: "bg-red-500 hover:bg-red-600 text-white",
+    primary: "bg-blue-500 hover:bg-blue-600 text-white",
+    outline: "border border-gray-500 text-gray-700 bg-white hover:bg-gray-100",
+    ghost: "bg-transparent text-gray-700 hover:bg-gray-200",
+  };
+
+  const sizeStyles = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
   };
 
   return (
     <button
-      className={`px-4 py-2 rounded-xl ${variantStyles[variant]} ${className}`}
+      className={`rounded-xl ${variantStyles[variant]} ${sizeStyles[size]} ${className}`}
       {...props}
     >
       {children}
